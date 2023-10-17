97ef510b4fbf5fb72aaffa434d416fca2dd5bae5
Marius Meissner <marius.meissner@atlasaero.eu>
2023-10-17 17:22:19 +0200

    Implemented brake time definition doctype

---
+++ hr_time/hr_time_management/doctype/brake_time_definition/brake_time_definition.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, AtlasAero GmbH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Brake time definition', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ hr_time/hr_time_management/doctype/brake_time_definition/test_brake_time_definition.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, AtlasAero GmbH and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBraketimedefinition(FrappeTestCase):
+	pass
