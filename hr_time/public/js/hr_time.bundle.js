b385a9bb51cd8dcc427952b0f668efb7acdfdcf2
Marius Meissner <marius.meissner@atlasaero.eu>
2023-10-31 13:46:34 +0100

    #3: Adjusted box shadow of dashboard widgets

--- hr_time/public/js/hr_time.bundle.js
+++ hr_time/public/js/hr_time.bundle.js
@@ -1,12 +1,5 @@
 import {EasyCheckinDialog} from "./easy_checkin_dialog";
 import {EasyCheckinStatus} from "./easy_checkin_status";
-import {NumberCardUtils} from "./utils";
-
-document.hr_time = {
-    utils: {
-        number_card: NumberCardUtils
-    }
-}
 
 document.bind_dashboard_easy_checkin = () => {
     EasyCheckinDialog.prepare_dashboard()
