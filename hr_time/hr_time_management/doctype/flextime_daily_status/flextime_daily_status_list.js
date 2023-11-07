dcba665d65419a4b469e8bd5f5fa3b0278b70b90
Marius Meissner <marius.meissner@atlasaero.eu>
2023-11-07 16:33:46 +0100

    #8: Removed employee name of employee column in report view

--- hr_time/hr_time_management/doctype/flextime_daily_status/flextime_daily_status_list.js
+++ hr_time/hr_time_management/doctype/flextime_daily_status/flextime_daily_status_list.js
@@ -9,6 +9,19 @@ frappe.listview_settings['Flextime daily status'] = {
     // set this to true to apply indicator function on draft documents too
     has_indicator_for_draft: false,
 
+    refresh(view) {
+      $("a[data-doctype=\"Employee\"]").each(function () {
+          let element = $(this);
+          let text = element.html();
+
+          if (!text.includes(":")) {
+              return;
+          }
+
+          element.html(text.split(":")[0]);
+      })
+    },
+
     get_indicator(doc) {
         if ((doc.target_working_time === 0 || doc.target_working_time === undefined) && doc.total_working_hours === 0) {
             return [__("No workday"), "grey", "target_working_time,=,0"];
